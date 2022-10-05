dde3d0ebf8f86216e8ef3782d7d848dab334b591
William Emfinger <waemfinger@gmail.com>
2022-10-05 15:01:34 -0500

    added external libfmt dependency because gnu++20 doesnt have support for std::format yet. added logger and format components which wrap / use libfmt. added logger/example code which shows how to use the logger component.

---
+++ components/logger/example/main/main.cpp
@@ -0,0 +1,50 @@
+#include <chrono>
+#include <functional>
+#include <thread>
+
+#include "logger.hpp"
+
+using namespace std::chrono_literals;
+
+extern "C" void app_main(void) {
+  // we get access to fmt since we include logger.hpp
+  fmt::print("Stating logger example!\n");
+  auto logger_fn = []() {
+    // create logger
+    auto logger = espp::Logger({
+        .tag = "Thread 2",
+        .level = espp::Logger::Level::INFO
+      });
+    size_t loop_iteration{0};
+    while (true) {
+      // log - note: debug shouldn't be shown!
+      logger.debug("some debug info: {}", loop_iteration);
+      logger.info("some info: {}", loop_iteration);
+      logger.warn("some warning: {}", loop_iteration);
+      logger.error("some error: {}", loop_iteration);
+      // update loop variables
+      loop_iteration++;
+      // sleep
+      std::this_thread::sleep_for(300ms);
+    }
+  };
+  auto logger_thread = std::thread(logger_fn);
+
+  // create another logger
+  auto logger = espp::Logger({
+      .tag = "Thread 1",
+      .level = espp::Logger::Level::DEBUG
+    });
+  size_t loop_iteration{0};
+  while (true) {
+    // log
+    logger.debug("some debug info: {}", loop_iteration);
+    logger.info("some info: {}", loop_iteration);
+    logger.warn("some warning: {}", loop_iteration);
+    logger.error("some error: {}", loop_iteration);
+    // update loop variables
+    loop_iteration++;
+    // sleep
+    std::this_thread::sleep_for(1s);
+  }
+}
---
+++ components/logger/include/logger.hpp
@@ -0,0 +1,69 @@
+#pragma once
+
+#include <string>
+#include <string_view>
+
+#define FMT_HEADER_ONLY
+#include "fmt/color.h"
+#include "fmt/format.h"
+#include "fmt/ranges.h"
+
+namespace espp {
+  class Logger {
+  public:
+
+    enum class Level {
+      DEBUG, INFO, WARN, ERROR,
+    };
+
+    struct Config {
+      std::string_view tag;
+      Level level = Level::WARN;
+    };
+    Logger(const Config& config) : tag_(config.tag), level_(config.level) {}
+
+    void set_log_level(const Level level) { level_ = level; }
+
+    // from https://en.cppreference.com/w/cpp/utility/format/format
+    template <typename... Args>
+    std::string format(std::string_view rt_fmt_str, Args&&... args) {
+      return fmt::vformat(rt_fmt_str, fmt::make_format_args(args...));
+    }
+
+    // print log in GRAY if level is DEBUG or greater
+    template <typename... Args>
+    void debug(std::string_view rt_fmt_str, Args&&... args) {
+      if (level_ > Level::DEBUG) return;
+      auto msg = format(rt_fmt_str, std::forward<Args>(args)...);
+      fmt::print(fg(fmt::color::gray), "[{}/D]:{}\n", tag_, msg);
+    }
+
+    // print log in GREEN if level is INFO or greater
+    template <typename... Args>
+    void info(std::string_view rt_fmt_str, Args&&... args) {
+      if (level_ > Level::INFO) return;
+      auto msg = format(rt_fmt_str, std::forward<Args>(args)...);
+      fmt::print(fg(fmt::color::green), "[{}/I]:{}\n", tag_, msg);
+    }
+
+    // print log in YELLOW if level is WARN or greater
+    template <typename... Args>
+    void warn(std::string_view rt_fmt_str, Args&&... args) {
+      if (level_ > Level::WARN) return;
+      auto msg = format(rt_fmt_str, std::forward<Args>(args)...);
+      fmt::print(fg(fmt::color::yellow), "[{}/W]:{}\n", tag_, msg);
+    }
+
+    // print log in RED if level is ERROR or greater
+    template <typename... Args>
+    void error(std::string_view rt_fmt_str, Args&&... args) {
+      if (level_ > Level::ERROR) return;
+      auto msg = format(rt_fmt_str, std::forward<Args>(args)...);
+      fmt::print(fg(fmt::color::red), "[{}/E]:{}\n", tag_, msg);
+    }
+
+  protected:
+    std::string tag_;
+    std::atomic<Level> level_;
+  };
+}
