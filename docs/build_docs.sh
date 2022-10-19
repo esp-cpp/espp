fc176c836dcc174550528695fb9fd6876b2c70ff
William Emfinger <waemfinger@gmail.com>
2022-10-19 11:18:20 -0500

    update doc output folder to be doc

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -1,3 +1,3 @@
 # build-docs -t esp32 esp32s2 esp32c3 esp32s3 esp32h2 esp32c2 esp32c6 -l en --project-path ../
 build-docs -t esp32 -l en --project-path ../
-mv _build/en/esp32/html static
+mv _build/en/esp32/html ../doc
