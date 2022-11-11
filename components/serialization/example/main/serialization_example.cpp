2a3f010629683cb4dfc302af20f272a3b9794829
William Emfinger <waemfinger@gmail.com>
2022-11-11 13:54:50 -0600

    added documentation (hacky) to serialization component. added additional complex structure serialization example.

--- components/serialization/example/main/serialization_example.cpp
+++ components/serialization/example/main/serialization_example.cpp
@@ -12,7 +12,7 @@ extern "C" void app_main(void) {
     fmt::print("Starting serialization example!\n");
     //! [serialization example]
     struct MyStruct {
-      uint8_t value;
+      uint8_t value; // NOTE: you cannot use int, you must use strongly sized types.
     };
     std::error_code ec;
     MyStruct object{5};
@@ -74,6 +74,38 @@ extern "C" void app_main(void) {
       }
     }
     //! [serialization example]
+  }
+
+  {
+    fmt::print("Starting complex serialization example!\n");
+    //! [complex serialization example]
+    struct MyStruct {
+      uint8_t value;
+      std::vector<uint32_t> data;
+      std::string name;
+    };
+    std::error_code ec;
+    MyStruct object{
+      42,
+      {1,3,3,7},
+      "the meaning of life"
+    };
+    std::vector<uint8_t> buffer;
+    auto bytes_written = espp::serialize(object, buffer);
+    fmt::print("Serialized {}B to std::vector\n", bytes_written);
+    auto new_object = espp::deserialize<MyStruct>(buffer, ec);
+    if (!ec && new_object.value == object.value) {
+      fmt::print("Deserialized successfully!\n");
+      fmt::print("\tvalue: {}\n"
+                 "\tdata:  {}\n"
+                 "\tname:  {}\n",
+                 new_object.value,
+                 new_object.data,
+                 new_object.name);
+    } else {
+      fmt::print("Deserialization failed: {}\n", ec.message());
+    }
+    //! [complex serialization example]
   }
 
   fmt::print("Serialization example complete!\n");
