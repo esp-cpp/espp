1af328f87ecccff685ace641da8f5df0550a9548
William Emfinger <waemfinger@gmail.com>
2023-03-30 11:52:33 -0500

    feat(math): update math formatting * Updated vector2d code to support printing / formatting using the libfmt library instead of hardcoding a to_string() method * Updated rangemapper to allow libfmt to format/print the config structure

--- components/math/include/range_mapper.hpp
+++ components/math/include/range_mapper.hpp
@@ -161,3 +161,40 @@ namespace espp {
    */
   typedef RangeMapper<int> IntRangeMapper;
 }
+
+#include "fmt/format.h"
+
+// NOTE: right now it seems we cannot use the generic version that works in
+//       vector2d.hpp because it results in 'template class without a name'
+//       errors...
+
+template<>
+struct fmt::formatter<espp::FloatRangeMapper::Config> : fmt::formatter<std::string>
+{
+  auto format(const espp::FloatRangeMapper::Config& config, format_context& ctx) {
+    return format_to(ctx.out(), "[{},{},{},{},{},{},{},{}]",
+                     config.center,
+                     config.deadband,
+                     config.minimum,
+                     config.maximum,
+                     config.invert_input,
+                     config.output_center,
+                     config.output_range,
+                     config.invert_output);
+  }
+};
+template<>
+struct fmt::formatter<espp::IntRangeMapper::Config> : fmt::formatter<std::string>
+{
+  auto format(const espp::IntRangeMapper::Config& config, format_context& ctx) {
+    return format_to(ctx.out(), "[{},{},{},{},{},{},{},{}]",
+                     config.center,
+                     config.deadband,
+                     config.minimum,
+                     config.maximum,
+                     config.invert_input,
+                     config.output_center,
+                     config.output_range,
+                     config.invert_output);
+  }
+};
