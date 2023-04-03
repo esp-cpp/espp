75ecc0e33d5c53b785bec73f00affe42419fd297
William Emfinger <waemfinger@gmail.com>
2023-04-03 16:08:17 -0500

    feat(joystick): add libfmt support * Added libfmt formatter to joystick component - supporting v (value), r (raw), and b (both value and raw) format specifiers * Updated joystick example to use libfmt support functions by just printing the joysticks.

--- components/joystick/include/joystick.hpp
+++ components/joystick/include/joystick.hpp
@@ -157,6 +157,8 @@ namespace espp {
       return raw_;
     }
 
+    friend struct fmt::formatter<Joystick>;
+
   protected:
     Vector2f raw_;
     Vector2f position_;
@@ -168,3 +170,42 @@ namespace espp {
     Logger logger_;
   };
 }
+
+
+// for allowing easy serialization/printing of the
+// Trigger class
+template<>
+struct fmt::formatter<espp::Joystick>
+{
+  // Presentation format: 'v' - value, 'r' - raw, 'b' - both.
+  char presentation = 'v';
+
+  // Parses format specifications of the form ['v' | 'r' | 'b'].
+  template<typename ParseContext>
+  constexpr auto parse(ParseContext& ctx) {
+    // Parse the presentation format and store it in the formatter:
+    auto it = ctx.begin(), end = ctx.end();
+    if (it != end && (*it == 'v' || *it == 'r' || *it == 'b')) presentation = *it++;
+
+    // TODO: Check if reached the end of the range:
+    // if (it != end && *it != '}') throw format_error("invalid format");
+
+    // Return an iterator past the end of the parsed range:
+    return it;
+  }
+
+  template<typename FormatContext>
+  auto format(espp::Joystick const& j, FormatContext& ctx) {
+    switch (presentation) {
+    case 'v':
+      return fmt::format_to(ctx.out(), "{}", j.position_);
+    case 'r':
+      return fmt::format_to(ctx.out(), "{}", j.raw_);
+    case 'b':
+      return fmt::format_to(ctx.out(), "({} -> {})", j.raw_, j.position_);
+    default:
+      // shouldn't get here!
+      return fmt::format_to(ctx.out(), "{}", j.position_);
+    }
+  }
+};
