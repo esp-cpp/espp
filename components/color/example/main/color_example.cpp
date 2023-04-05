8e06088d0c7ca2208583488327b0854ec993ea76
William Emfinger <waemfinger@gmail.com>
2023-04-05 15:25:54 -0500

    feat(color): added component * Added color component with Rgb and Hsv classes * Added example showing conversion as well as blending

---
+++ components/color/example/main/color_example.cpp
@@ -0,0 +1,46 @@
+#include <chrono>
+#include <vector>
+#include <thread>
+
+#include "format.hpp"
+#include "color.hpp"
+
+using namespace std::chrono_literals;
+
+extern "C" void app_main(void) {
+  {
+    fmt::print("Starting color example!\n");
+    //! [color example]
+    for (float h = 0.0f; h < 360.0f; h += 10.0f) {
+        espp::Hsv hsv(h, 1.0f, 1.0f);
+        espp::Rgb rgb(hsv);
+        // NOTE: hue values: red = 0, green = 120, blue = 240
+        fmt::print("HSV {} --> RGB {}\n", hsv, rgb);
+    }
+    // Test color blending
+    espp::Rgb white (1., 1., 1.);
+    espp::Rgb black (0., 0., 0.);
+    espp::Rgb red   (1., 0., 0.);
+    espp::Rgb green (0., 1., 0.);
+    espp::Rgb blue  (0., 0., 1.);
+    // red + black = dark red
+    auto dark_red = red + black;
+    fmt::print("Dark Red: {}\n", dark_red);
+    // red + green = dark yellow
+    auto dark_yellow = red + green;
+    fmt::print("Dark Yellow: {}\n", dark_yellow);
+    // red + white = pink
+    auto pink = red + white;
+    fmt::print("Pink: {}\n", pink);
+    // black + white = grey
+    auto grey = black + white;
+    fmt::print("Grey: {}\n", grey);
+    //! [color example]
+  }
+
+  fmt::print("Cli example complete!\n");
+
+  while (true) {
+    std::this_thread::sleep_for(1s);
+  }
+}
---
+++ components/color/src/color.cpp
@@ -0,0 +1,113 @@
+#include "color.hpp"
+
+namespace espp {
+
+  Rgb::Rgb(const float& _r, const float& _g, const float& _b) : r(_r), g(_g), b(_b) {}
+
+  Rgb::Rgb(const Rgb& rgb) : r(rgb.r), g(rgb.g), b(rgb.b) {}
+
+  Rgb::Rgb(const Hsv& hsv) : Rgb(hsv.rgb()) {}
+
+  Rgb Rgb::operator+ (const Rgb& rhs) const {
+    // divide by number of elements that went into it (blending) instead of just
+    // addition (which would be more like the light model) so that we are better
+    // able to create gradients.
+    return Rgb(
+      (r + rhs.r)/2.0f,
+      (g + rhs.g)/2.0f,
+      (b + rhs.b)/2.0f
+    );
+  }
+
+  Rgb& Rgb::operator+= (const Rgb& rhs) {
+    r += rhs.r;
+    g += rhs.g;
+    b += rhs.b;
+    // divide by number of elements that went into it (blending) instead of just
+    // addition (which would be more like the light model) so that we are better
+    // able to create gradients.
+    r /= 2.0f;
+    g /= 2.0f;
+    b /= 2.0f;
+    return *this;
+  }
+
+  Hsv Rgb::hsv() const {
+    Hsv HSV;
+
+    float
+      max = std::max(r, std::max(g, b)),
+      min = std::min(r, std::min(g, b)),
+      d;
+
+    HSV.v = max;
+    if (max == 0.0f) {
+      HSV.s = 0.0;
+    } else {
+      HSV.s = (max-min)/max;
+    }
+    if (HSV.s == 0.0f) {
+      // NOTE: H is undefined here, so we will say H is angle -1, outside
+      //       the range [0,360].
+      HSV.h = -1.0f;
+    } else {
+      d = max - min;
+      if (r == max) {
+        // color is between yellow and magenta
+        HSV.h = (g-b)/d;
+      } else if (g == max) {
+        // color is between cyan and yellow
+        HSV.h = 2.0f + (b-r)/d;
+      } else if (b == max) {
+        // color is between magenta and cyan
+        HSV.h = 4.0f + (r-g)/d;
+      }
+      // convert to degrees
+      HSV.h *= 60.0f;
+      // prevent negative values
+      if (HSV.h < 0) {
+        HSV.h += 360.0f;
+      }
+    }
+
+    return HSV;
+  }
+
+  Hsv::Hsv(const float& _h, const float& _s, const float & _v) : h(_h), s(_s), v(_v) {}
+
+  Hsv::Hsv(const Hsv& hsv) : h(hsv.h), s(hsv.s), v(hsv.v) {}
+
+  Hsv::Hsv(const Rgb& rgb) : Hsv(rgb.hsv()) {}
+
+  Rgb Hsv::rgb() const {
+    Rgb RGB;
+
+    float H = h, S = s, V = v,
+      P, Q, T,
+      fract;
+
+    (H == 360.)?(H = 0.):(H /= 60.);
+    fract = H - floor(H);
+
+    P = V*(1. - S);
+    Q = V*(1. - S*fract);
+    T = V*(1. - S*(1. - fract));
+
+    if (0. <= H && H < 1.)
+      RGB = Rgb(V, T, P);
+    else if (1. <= H && H < 2.)
+      RGB = Rgb(Q, V, P);
+    else if (2. <= H && H < 3.)
+      RGB = Rgb(P, V, T);
+    else if (3. <= H && H < 4.)
+      RGB = Rgb(P, Q, V);
+    else if (4. <= H && H < 5.)
+      RGB = Rgb(T, P, V);
+    else if (5. <= H && H < 6.)
+      RGB = Rgb(V, P, Q);
+    else
+      RGB = Rgb(0., 0., 0.);
+
+    return RGB;
+  }
+} // namespace espp
