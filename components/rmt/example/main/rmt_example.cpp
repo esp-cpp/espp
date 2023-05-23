d24a460a609d232b6e56a394ed75f83cfbcac266
William Emfinger <waemfinger@gmail.com>
2023-05-23 16:23:24 -0500

    feat(rmt): added rmt component * Added rmt component with Rmt and RmtEncoder classes * Added example showing the use of the Rmt component to control RGB LED on the TinyS3 * Updated docs * Updated ci

---
+++ components/rmt/example/main/rmt_example.cpp
@@ -0,0 +1,158 @@
+#include <chrono>
+#include <vector>
+
+#include <driver/gpio.h>
+
+#include "color.hpp"
+#include "logger.hpp"
+#include "rmt.hpp"
+#include "task.hpp"
+
+using namespace std::chrono_literals;
+
+extern "C" void app_main(void) {
+  // create a logger
+  espp::Logger logger({.tag = "rmt example", .level = espp::Logger::Verbosity::INFO});
+  {
+    // configure the power pin (TinyS3)
+    static constexpr int led_power_pin = 17;
+    gpio_config_t power_pin_config = {
+        .pin_bit_mask = (1ULL << led_power_pin),
+        .mode = GPIO_MODE_OUTPUT,
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .intr_type = GPIO_INTR_DISABLE,
+    };
+    gpio_config(&power_pin_config);
+    // turn on the power
+    gpio_set_level((gpio_num_t)led_power_pin, 1);
+
+    //! [rmt encoder example]
+    //
+    // The RmtEncoder provides a way to encode data into the RMT peripheral.
+    // This code is a custom encoder that encodes WS2812B data. It uses two
+    // encoders, a bytes encoder and a copy encoder. The bytes encoder encodes
+    // the RGB data into the RMT peripheral and the copy encoder encodes the
+    // reset code. The reset code is a special code that is sent after the RGB
+    // data to reset the WS2812B LEDs. The reset code is a 50us low pulse
+    // followed by a 50us high pulse. The reset code is sent after the RGB data
+    // to ensure that the WS2812B LEDs latch the RGB data. The reset code is
+    // sent after the RGB data because the WS2812B LEDs latch the RGB data on
+    // the rising edge of the reset code.
+    //
+    // This code is copied from the led_stip example in the esp-idf
+    // (https://github.com/espressif/esp-idf/tree/master/examples/peripherals/rmt/led_strip/main)
+    int led_encoder_state = 0;
+    auto led_encoder = std::make_unique<espp::RmtEncoder>(espp::RmtEncoder::Config{
+        .bytes_encoder_config = espp::RmtEncoder::ws2812_bytes_encoder_config,
+        .encode = [&led_encoder_state](auto channel, auto *copy_encoder, auto *bytes_encoder,
+                                       const void *data, size_t data_size,
+                                       rmt_encode_state_t *ret_state) -> size_t {
+          static uint16_t reset_ticks =
+              10000000 / 1000000 * 50 / 2; // reset code duration defaults to 50us
+          static rmt_symbol_word_t led_reset_code = (rmt_symbol_word_t){
+              .duration0 = reset_ticks,
+              .level0 = 0,
+              .duration1 = reset_ticks,
+              .level1 = 0,
+          };
+          rmt_encode_state_t session_state = RMT_ENCODING_RESET;
+          int state = RMT_ENCODING_RESET;
+          size_t encoded_symbols = 0;
+          switch (led_encoder_state) {
+          case 0: // send RGB data
+            encoded_symbols +=
+                bytes_encoder->encode(bytes_encoder, channel, data, data_size, &session_state);
+            if (session_state & RMT_ENCODING_COMPLETE) {
+              led_encoder_state = 1; // switch to next state when current encoding session finished
+            }
+            if (session_state & RMT_ENCODING_MEM_FULL) {
+              state |= RMT_ENCODING_MEM_FULL;
+              goto out; // yield if there's no free space for encoding artifacts
+            }
+            // fall-through
+          case 1: // send reset code
+            encoded_symbols += copy_encoder->encode(copy_encoder, channel, &led_reset_code,
+                                                    sizeof(led_reset_code), &session_state);
+            if (session_state & RMT_ENCODING_COMPLETE) {
+              led_encoder_state = RMT_ENCODING_RESET; // back to the initial encoding session
+              state |= RMT_ENCODING_COMPLETE;
+            }
+            if (session_state & RMT_ENCODING_MEM_FULL) {
+              state |= RMT_ENCODING_MEM_FULL;
+              goto out; // yield if there's no free space for encoding artifacts
+            }
+          }
+        out:
+          *ret_state = static_cast<rmt_encode_state_t>(state);
+          return encoded_symbols;
+        },
+        .del = [](auto *base_encoder) -> esp_err_t {
+          // we don't have any extra resources to free, so just return ESP_OK
+          return ESP_OK;
+        },
+        .reset = [&led_encoder_state](auto *base_encoder) -> esp_err_t {
+          // all we have is some extra state to reset
+          led_encoder_state = 0;
+          return ESP_OK;
+        },
+    });
+    //! [rmt encoder example]
+
+    //! [rmt example]
+    // create the rmt object
+    espp::Rmt rmt(espp::Rmt::Config{
+        .gpio_num = 18, // WS2812B data pin on the TinyS3
+        .log_level = espp::Logger::Verbosity::INFO,
+    });
+
+    // tell the RMT object to use the led_encoder (espp::RmtEncoder) that's
+    // defined above
+    rmt.set_encoder(std::move(led_encoder));
+
+    // create a task to cycle through rainbow colors and send them to the
+    // WS2812B LED using the RMT peripheral
+    auto task_fn = [&rmt](std::mutex &m, std::condition_variable &cv) {
+      static auto start = std::chrono::high_resolution_clock::now();
+      auto now = std::chrono::high_resolution_clock::now();
+      float t = std::chrono::duration<float>(now - start).count();
+      // rotate through rainbow colors in hsv based on time, hue is 0-360
+      float hue = (cos(t) * 0.5f + 0.5f) * 360.0f;
+      espp::Hsv hsv(hue, 1.0f, 1.0f);
+      espp::Rgb rgb = hsv.rgb();
+      uint8_t green = std::clamp(int(rgb.g * 255), 0, 255);
+      uint8_t blue = std::clamp(int(rgb.b * 255), 0, 255);
+      uint8_t red = std::clamp(int(rgb.r * 255), 0, 255);
+      // NOTE: we only have one LED so we only need to send one set of RGB data
+      uint8_t data[3] = {green, blue, red};
+      // now we can send the data to the WS2812B LED
+      rmt.transmit(data, sizeof(data));
+      fmt::print("hsv->rgb->uint: {} -> {} -> {} {} {}\n", hsv, rgb, green, blue, red);
+      // NOTE: sleeping in this way allows the sleep to exit early when the
+      // task is being stopped / destroyed
+      {
+        std::unique_lock<std::mutex> lk(m);
+        cv.wait_for(lk, 50ms);
+      }
+      // don't want to stop the task
+      return false;
+    };
+
+    auto task = espp::Task({.name = "Rmt Task",
+                            .callback = task_fn,
+                            .stack_size_bytes = 5 * 1024,
+                            .log_level = espp::Logger::Verbosity::WARN});
+    task.start();
+    //! [rmt example]
+
+    while (true) {
+      std::this_thread::sleep_for(100ms);
+    }
+  }
+
+  fmt::print("Rmt example complete!\n");
+
+  while (true) {
+    std::this_thread::sleep_for(1s);
+  }
+}
---
+++ components/rmt/include/rmt_encoder.hpp
@@ -0,0 +1,182 @@
+#pragma once
+
+#include <functional>
+
+#include "driver/rmt_encoder.h"
+
+namespace espp {
+/// \brief Class representing an RMT encoder
+/// \details This class is used to encode data for the RMT peripheral.
+/// It is used by the Rmt class to encode data for transmission.
+/// \sa
+/// https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/peripherals/rmt.html#rmt-encoder
+///
+///  \section rmt_encoder_ex1 Example 1: WS2812 encoder
+///  \snippet rmt_example.cpp rmt encoder example
+class RmtEncoder {
+public:
+  /// \brief Configuration for the byte encoding for WS2812 LEDs
+  /// \details This configuration is used to encode bytes for WS2812 LEDs.
+  /// \note This configuration can be provided to the configuration for this
+  /// class.
+  /// \sa Config
+  static constexpr rmt_bytes_encoder_config_t ws2812_bytes_encoder_config = {
+      .bit0 =
+          {
+              // divide the rmt transmit resolution (10 MHz) by 1,000,000
+              .duration0 = static_cast<uint16_t>(0.3 * 10000000 / 1000000), // T0H=0.3us
+              .level0 = 1,
+              // divide the rmt transmit resolution (10 MHz) by 1,000,000
+              .duration1 = static_cast<uint16_t>(0.9 * 10000000 / 1000000), // T0L=0.9us
+              .level1 = 0,
+          },
+      .bit1 =
+          {
+              // divide the rmt transmit resolution (10 MHz) by 1,000,000
+              .duration0 = static_cast<uint16_t>(0.9 * 10000000 / 1000000), // T1H=0.9us
+              .level0 = 1,
+              // divide the rmt transmit resolution (10 MHz) by 1,000,000
+              .duration1 = static_cast<uint16_t>(0.3 * 10000000 / 1000000), // T1L=0.3us
+              .level1 = 0,
+          },
+      .flags =
+          {
+              .msb_first = 1 // WS2812 transfer bit order: G7...G0R7...R0B7...B0
+          },
+  };
+
+  /// \brief Function to encode data for the RMT peripheral
+  /// \param channel RMT channel to use for encoding
+  /// \param copy_encoder RMT encoder to use for copying data
+  /// \param bytes_encoder RMT encoder to use for encoding bytes
+  /// \param primary_data Pointer to the primary data to encode
+  /// \param data_size Size of the primary data to encode
+  /// \param ret_state Pointer to the RMT encoder state to return
+  /// \return Number of bytes encoded
+  /// \note This function is called by the Rmt class to encode data for
+  /// transmission. It is called repeatedly until all data has been encoded.
+  /// \note This function should return the number of bytes encoded.
+  /// \note This function should set the RMT encoder state to the next state
+  /// to be used for encoding.
+  typedef std::function<size_t(rmt_channel_handle_t channel, rmt_encoder_t *copy_encoder,
+                               rmt_encoder_t *bytes_encoder, const void *primary_data,
+                               size_t data_size, rmt_encode_state_t *ret_state)>
+      encode_fn;
+
+  /// \brief Function to delete an RMT encoder
+  /// \param encoder RMT encoder to delete
+  /// \return ESP_OK if the encoder was successfully deleted, an error code
+  typedef std::function<esp_err_t(rmt_encoder_t *)> delete_fn;
+
+  /// \brief Function to reset an RMT encoder
+  /// \param encoder RMT encoder to reset
+  /// \return ESP_OK if the encoder was successfully reset, an error code
+  typedef std::function<esp_err_t(rmt_encoder_t *)> reset_fn;
+
+  /// \brief Configuration for this class
+  struct Config {
+    rmt_bytes_encoder_config_t bytes_encoder_config; ///< Configuration for the RMT bytes encoder
+    encode_fn encode; ///< Function to encode data for the RMT peripheral
+    delete_fn del;    ///< Function to delete an RMT encoder
+    reset_fn reset;   ///< Function to reset an RMT encoder
+  };
+
+  /// \brief Constructor
+  /// \param config Configuration for this class
+  RmtEncoder(const Config &config) { init(config); }
+
+  /// \brief Destructor
+  ~RmtEncoder() {
+    if (handle_ != nullptr)
+      rmt_del_encoder(handle_);
+  }
+
+  /// \brief Get the RMT encoder handle
+  /// \return RMT encoder handle
+  rmt_encoder_handle_t handle() const { return handle_; }
+
+protected:
+  /// \brief RMT encoder wrapper
+  struct Wrapper {
+    rmt_encoder_t base_;           ///< Base RMT encoder
+    rmt_encoder_t *bytes_encoder_; ///< RMT encoder for encoding bytes
+    rmt_encoder_t *copy_encoder_;  ///< RMT encoder for copying data
+    RmtEncoder *self_;             ///< Pointer to the RmtEncoder object
+  };
+
+  static size_t encoder_encode(rmt_encoder_t *encoder, rmt_channel_handle_t channel,
+                               const void *primary_data, size_t data_size,
+                               rmt_encode_state_t *ret_state) {
+    // get the encoders from the wrapper
+    Wrapper *wrapper = __containerof(encoder, Wrapper, base_);
+    if (wrapper == nullptr) {
+      return 0;
+    }
+    RmtEncoder *self = wrapper->self_;
+    if (self == nullptr) {
+      return 0;
+    }
+    rmt_encoder_t *copy_encoder = wrapper->copy_encoder_;
+    rmt_encoder_t *bytes_encoder = wrapper->bytes_encoder_;
+    // call the object's encode function
+    auto bytes_encoded =
+        self->encode_(channel, copy_encoder, bytes_encoder, primary_data, data_size, ret_state);
+    return bytes_encoded;
+  }
+
+  static esp_err_t encoder_reset(rmt_encoder_t *encoder) {
+    // get the encoders from the wrapper
+    Wrapper *wrapper = __containerof(encoder, Wrapper, base_);
+    RmtEncoder *self = wrapper->self_;
+    // call the object's reset runction
+    self->reset_(encoder);
+    // now reset the encoders
+    rmt_encoder_reset(wrapper->bytes_encoder_);
+    rmt_encoder_reset(wrapper->copy_encoder_);
+    return ESP_OK;
+  }
+
+  static esp_err_t encoder_delete(rmt_encoder_t *encoder) {
+    // get the encoders from the wrapper
+    Wrapper *wrapper = __containerof(encoder, Wrapper, base_);
+    if (wrapper == nullptr) {
+      return ESP_OK;
+    }
+    RmtEncoder *self = wrapper->self_;
+    // call the object's delete function
+    if (self->del_ != nullptr)
+      self->del_(encoder);
+    // delete the encoders
+    rmt_del_encoder(wrapper->bytes_encoder_);
+    rmt_del_encoder(wrapper->copy_encoder_);
+    // delete the wrapper
+    delete wrapper;
+    // now delete the RmtEncoder object
+    delete self;
+    return ESP_OK;
+  }
+
+  void init(const Config &config) {
+    encode_ = config.encode;
+    del_ = config.del;
+    reset_ = config.reset;
+    // create the wrapper
+    Wrapper *wrapper = new Wrapper;
+    wrapper->self_ = this;
+    // create the encoders
+    wrapper->base_.encode = encoder_encode;
+    wrapper->base_.del = encoder_delete;
+    wrapper->base_.reset = encoder_reset;
+    rmt_new_bytes_encoder(&config.bytes_encoder_config, &wrapper->bytes_encoder_);
+    rmt_copy_encoder_config_t copy_encoder_config = {};
+    rmt_new_copy_encoder(&copy_encoder_config, &wrapper->copy_encoder_);
+    // store the handle
+    handle_ = &wrapper->base_;
+  }
+
+  encode_fn encode_;
+  delete_fn del_;
+  reset_fn reset_;
+  rmt_encoder_handle_t handle_{nullptr};
+};
+} // namespace espp
