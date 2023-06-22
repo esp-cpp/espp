2d48130ac03b28099de53c06cb47446f4a7725c7
William Emfinger <waemfinger@gmail.com>
2023-06-22 16:13:27 -0500

    feat(button): update Button implementation * Removed event manager and serialization from button component and class, in favor of simply calling a callback function * Updated button example to use the new callback API but to include event manager example for button subscription / data distribution example since many systems may have multiple comonents that are interested in button presses. Uses new event manager API as well

--- components/button/example/main/button_example.cpp
+++ components/button/example/main/button_example.cpp
@@ -3,6 +3,7 @@
 
 #include "button.hpp"
 #include "event_manager.hpp"
+#include "serialization.hpp"
 
 using namespace std::chrono_literals;
 
@@ -18,12 +19,28 @@ extern "C" void app_main(void) {
   // create a button on GPIO 2
   espp::Button button({
       .gpio_num = GPIO_NUM_2,
-      .topic = button_topic,
-      .component_name = button_component_name,
+      .callback =
+          [&](const espp::Button::Event &event) {
+            auto now = std::chrono::high_resolution_clock::now();
+            auto elapsed = std::chrono::duration<float>(now - start).count();
+            logger.info("[Callback][{:.3f}] Button {} state changed to: {}", elapsed,
+                        event.gpio_num, event.pressed);
+            // serialize the event
+            std::error_code ec;
+            std::vector<uint8_t> buffer;
+            auto bytes_written = espp::serialize(event, buffer);
+            if (bytes_written > 0) {
+              // publish the event
+              espp::EventManager::get().publish(button_topic, buffer);
+            } else {
+              logger.error("Failed to serialize button state");
+            }
+          },
       .active_level = espp::Button::ActiveLevel::HIGH,
       .interrupt_type = espp::Button::InterruptType::ANY_EDGE,
       .pullup_enabled = false,
       .pulldown_enabled = false,
+      .log_level = espp::Logger::Verbosity::WARN,
   });
 
   logger.info("Initial button state: {}", button.is_pressed());
@@ -31,15 +48,15 @@ extern "C" void app_main(void) {
   // register subscriber on the button topic
   auto &em = espp::EventManager::get();
   auto did_sub =
-      em.add_subscriber(button_topic, "example subscriber", [&](const std::string &data) {
+      em.add_subscriber(button_topic, "example subscriber", [&](const std::vector<uint8_t> &data) {
         auto now = std::chrono::high_resolution_clock::now();
         auto elapsed = std::chrono::duration<float>(now - start).count();
         // deserialize the data
         std::error_code ec;
         auto state = espp::deserialize<espp::Button::Event>(data, ec);
         if (!ec) {
-          logger.info("[{:.3f}]: button {} state changed to: {}", elapsed, state.gpio_num,
-                      state.pressed);
+          logger.info("[Subscriber][{:.3f}]: button {} state changed to: {}", elapsed,
+                      state.gpio_num, state.pressed);
         } else {
           logger.error("Failed to deserialize button state: {}", ec.message());
         }
