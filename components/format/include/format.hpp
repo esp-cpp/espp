57b5b0c0a967915173210537c854bd7ee678fff9
William Emfinger <waemfinger@gmail.com>
2023-11-25 10:31:09 -0600

    Update format for latest gcc and esp-idf (#130)
    * fix(format): update format to add additional headers and to fix printing of std::atomic
    
    * feat(fmt): update format to latest and add fmt/std.h include
    
    * revert tt21100 change

--- components/format/include/format.hpp
+++ components/format/include/format.hpp
@@ -1,8 +1,13 @@
 #pragma once
 
+#include <string>
+
 #define FMT_HEADER_ONLY
 #include <fmt/color.h>
 #include <fmt/format.h>
 #include <fmt/ranges.h>
+#include <fmt/std.h>
 // to print time
 #include <fmt/chrono.h>
+// to print files
+#include <fmt/ostream.h>
