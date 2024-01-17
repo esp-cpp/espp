77149faf674695358951ec6c32bb93b4883d8b4f
William Emfinger <waemfinger@gmail.com>
2024-01-17 13:34:35 -0600

    example(i2c): update example for configurability * Remove unnecessary task * Switch fmt::print to logger * Allow menuconfig configuration of the device address, register address, and register size for the test

--- components/i2c/example/main/i2c_example.cpp
+++ components/i2c/example/main/i2c_example.cpp
@@ -3,55 +3,36 @@
 #include <vector>
 
 #include "i2c.hpp"
-#include "task.hpp"
+#include "logger.hpp"
 
 using namespace std::chrono_literals;
 
 extern "C" void app_main(void) {
-  {
-    std::atomic<bool> quit_test = false;
-    fmt::print("Starting ft5x06 example, press select & start together to quit!\n");
-    //! [i2c example]
-    espp::I2c i2c({
-        .port = I2C_NUM_0,
-        .sda_io_num = (gpio_num_t)CONFIG_EXAMPLE_I2C_SDA_GPIO,
-        .scl_io_num = (gpio_num_t)CONFIG_EXAMPLE_I2C_SCL_GPIO,
+  espp::Logger logger({.tag = "I2C Example", .level = espp::Logger::Verbosity::INFO});
+  logger.info("Starting");
+  //! [i2c example]
+  espp::I2c i2c({
+      .port = I2C_NUM_0,
+      .sda_io_num = (gpio_num_t)CONFIG_EXAMPLE_I2C_SDA_GPIO,
+      .scl_io_num = (gpio_num_t)CONFIG_EXAMPLE_I2C_SCL_GPIO,
     });
-    // finally, make the task to periodically query the bus
-    auto task_fn = [&i2c](std::mutex &m, std::condition_variable &cv) {
-      static constexpr uint8_t device_address = 0x58;
-      static constexpr uint8_t register_address = 0x58;
-      bool device_found = i2c.probe_device(device_address);
-      if (device_found) {
-        fmt::print("Found device with address 0x{:#02x}\n", device_address);
-        uint8_t read_data[1];
-        bool success = i2c.read_at_register(device_address, register_address, read_data, 1);
-        if (success) {
-          fmt::print("read data: {:#04x}\n", read_data[0]);
-        } else {
-          fmt::print("read failed\n");
-        }
-      } else {
-        fmt::print("Could not find device with address 0x{:#02x}\n", device_address);
-      }
-      // NOTE: sleeping in this way allows the sleep to exit early when the
-      // task is being stopped / destroyed
-      {
-        std::unique_lock<std::mutex> lk(m);
-        cv.wait_for(lk, 500ms);
-      }
-      return false; // don't stop the task
-    };
-    auto task = espp::Task(
-        {.name = "I2c Task", .callback = task_fn, .log_level = espp::Logger::Verbosity::WARN});
-    task.start();
-    //! [i2c example]
-    while (!true) {
-      std::this_thread::sleep_for(100ms);
+  static constexpr uint8_t device_address = CONFIG_EXAMPLE_I2C_DEVICE_ADDR;
+  static constexpr uint8_t register_address = CONFIG_EXAMPLE_I2C_DEVICE_REG_ADDR;
+  bool device_found = i2c.probe_device(device_address);
+  if (device_found) {
+    logger.info("Found device with address {:#02x}", device_address);
+    std::vector<uint8_t> read_data(CONFIG_EXAMPLE_I2C_DEVICE_REG_SIZE, 0);
+    bool success = i2c.read_at_register(device_address, register_address, read_data.data(), read_data.size());
+    if (success) {
+      logger.info("read data: {::#02x}", read_data);
+    } else {
+      logger.error("read failed");
     }
+  } else {
+    logger.error("Could not find device with address {:#02x}", device_address);
   }
-
-  fmt::print("Ft5x06 example complete!\n");
+  //! [i2c example]
+  logger.info("I2C example complete!");
 
   while (true) {
     std::this_thread::sleep_for(1s);
