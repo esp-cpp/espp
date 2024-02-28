9b0b77e8d2805b0d9046079defa259469be3040d
William Emfinger <waemfinger@gmail.com>
2024-02-28 16:32:22 -0600

    fix(docs): add definitions to doxyfile for ble server to get included (#158)

--- components/ble_gatt_server/include/device_info_service.hpp
+++ components/ble_gatt_server/include/device_info_service.hpp
@@ -8,7 +8,7 @@
 #include <string>
 #include <vector>
 
-#if CONFIG_BT_NIMBLE_ENABLED
+#if CONFIG_BT_NIMBLE_ENABLED || defined(_DOXYGEN_)
 
 #include "NimBLEDevice.h"
 
@@ -210,4 +210,4 @@ protected:
 };
 } // namespace espp
 
-#endif // CONFIG_BT_NIMBLE_ENABLED
+#endif // CONFIG_BT_NIMBLE_ENABLED || defined(_DOXYGEN_)
