4c842ae39a51314316ca0a063ea65a2103f1ac5f
William Emfinger <waemfinger@gmail.com>
2024-03-11 12:54:36 -0500

    feat(button): refactor button class to be a subclass of interrupt * Update so callback-based functionality of button uses the interrupt subclass functionality * Add additional API so button can be a simple wrapper around gpio management (without callback or interrupt) as well

--- components/button/example/main/button_example.cpp
+++ components/button/example/main/button_example.cpp
@@ -11,35 +11,62 @@ extern "C" void app_main(void) {
   espp::Logger logger({.tag = "Button Example", .level = espp::Logger::Verbosity::INFO});
   logger.info("Starting example!");
 
+  //! [simple button example]
+  // create the button
+  espp::Button simple_button({
+      .name = "Button 12",
+      .gpio_num = GPIO_NUM_12,
+      .active_level = espp::Interrupt::ActiveLevel::LOW,
+      .pullup_enabled = false,
+      .pulldown_enabled = false,
+      .log_level = espp::Logger::Verbosity::WARN,
+  });
+
+  for (int i = 0; i < 10; i++) {
+    logger.info("Button 12 state: {}", simple_button.is_pressed());
+    std::this_thread::sleep_for(1s);
+  }
+  //! [simple button example]
+
   //! [button example]
   static auto start = std::chrono::high_resolution_clock::now();
   std::string button_topic = "button/state";
   std::string button_component_name = "button";
 
-  // create a button on GPIO 2
+  // create a button
   espp::Button button({
-      .gpio_num = GPIO_NUM_2,
-      .callback =
-          [&](const espp::Button::Event &event) {
-            auto now = std::chrono::high_resolution_clock::now();
-            auto elapsed = std::chrono::duration<float>(now - start).count();
-            logger.info("[Callback][{:.3f}] Button {} state changed to: {}", elapsed,
-                        event.gpio_num, event.pressed);
-            // serialize the event
-            std::error_code ec;
-            std::vector<uint8_t> buffer;
-            auto bytes_written = espp::serialize(event, buffer);
-            if (bytes_written > 0) {
-              // publish the event
-              espp::EventManager::get().publish(button_topic, buffer);
-            } else {
-              logger.error("Failed to serialize button state");
-            }
+      .name = "Button 12",
+      .interrupt_config =
+          {
+              .gpio_num = GPIO_NUM_12,
+              .callback =
+                  [&](const espp::Interrupt::Event &event) {
+                    auto now = std::chrono::high_resolution_clock::now();
+                    auto elapsed = std::chrono::duration<float>(now - start).count();
+                    logger.info("[Callback][{:.3f}] Button {} state changed to: {}", elapsed,
+                                event.gpio_num, event.active);
+                    // serialize the event
+                    std::error_code ec;
+                    std::vector<uint8_t> buffer;
+                    auto bytes_written = espp::serialize(event, buffer);
+                    if (bytes_written > 0) {
+                      // publish the event
+                      espp::EventManager::get().publish(button_topic, buffer);
+                    } else {
+                      logger.error("Failed to serialize button state");
+                    }
+                  },
+              .active_level = espp::Interrupt::ActiveLevel::LOW,
+              .interrupt_type = espp::Interrupt::Type::ANY_EDGE,
+              .pullup_enabled = false,
+              .pulldown_enabled = false,
           },
-      .active_level = espp::Button::ActiveLevel::HIGH,
-      .interrupt_type = espp::Button::InterruptType::ANY_EDGE,
-      .pullup_enabled = false,
-      .pulldown_enabled = false,
+      .task_config =
+          {
+              .name = "Button 12 task",
+              .stack_size_bytes = 8192,
+              .priority = 5,
+          },
       .log_level = espp::Logger::Verbosity::WARN,
   });
 
@@ -53,10 +80,10 @@ extern "C" void app_main(void) {
         auto elapsed = std::chrono::duration<float>(now - start).count();
         // deserialize the data
         std::error_code ec;
-        auto state = espp::deserialize<espp::Button::Event>(data, ec);
+        auto state = espp::deserialize<espp::Interrupt::Event>(data, ec);
         if (!ec) {
           logger.info("[Subscriber][{:.3f}]: button {} state changed to: {}", elapsed,
-                      state.gpio_num, state.pressed);
+                      state.gpio_num, state.active);
         } else {
           logger.error("Failed to deserialize button state: {}", ec.message());
         }
@@ -65,15 +92,26 @@ extern "C" void app_main(void) {
 
   // create another button
   espp::Button button_2({
-      .gpio_num = GPIO_NUM_37,
-      .callback =
-          [&](const espp::Button::Event &event) {
-            logger.info("Button {} state changed to: {}", event.gpio_num, event.pressed);
+      .name = "Button 0",
+      .interrupt_config =
+          {
+              .gpio_num = GPIO_NUM_0,
+              .callback =
+                  [&](const espp::Interrupt::Event &event) {
+                    logger.info("Button {} state changed to: {}", event.gpio_num, event.active);
+                  },
+              .active_level = espp::Interrupt::ActiveLevel::LOW,
+              .interrupt_type = espp::Interrupt::Type::ANY_EDGE,
+              .pullup_enabled = false,
+              .pulldown_enabled = false,
+              .enable_pin_glitch_filter = true,
           },
-      .active_level = espp::Button::ActiveLevel::HIGH,
-      .interrupt_type = espp::Button::InterruptType::ANY_EDGE,
-      .pullup_enabled = false,
-      .pulldown_enabled = false,
+      .task_config =
+          {
+              .name = "Button 0 task",
+              .stack_size_bytes = 8192,
+              .priority = 5,
+          },
       .log_level = espp::Logger::Verbosity::WARN,
   });
 
