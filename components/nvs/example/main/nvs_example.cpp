d10457cdf3a2535a32d7daae9eb7d1641df921e7
SamAtBackbone <sam@playbackbone.com>
2024-05-15 16:27:17 -0700

    Add NVS Example to code.

---
+++ components/nvs/example/main/nvs_example.cpp
@@ -0,0 +1,30 @@
+#include <bitset>
+#include <chrono>
+#include <thread>
+#include <vector>
+
+#include "format.hpp"
+#include "nvs.hpp"
+
+using namespace std::chrono_literals;
+
+extern "C" void app_main(void) {
+    std::this_thread::sleep_for(200ms);
+    fmt::print("Starting NVS example!\n");
+    //! [NVS example]
+    std::error_code ec;
+    uint8_t counter = 0;
+    espp::Nvs nvs;
+    //note that the namespace and key strings must be shorter than 15 characters
+    nvs.get_nvs_var("system", "reset_counter", counter, counter, ec); 
+    ec.clear();
+    fmt::print("Reset Counter = {}\n", counter);
+    nvs.set_nvs_var("system", "reset_counter", ++counter, ec);
+    fmt::print("Next Reset Counter will be = {}\n", counter);
+    fmt::print("NVS example complete!\n");
+    //! [NVS example]
+
+    while (true) {
+        std::this_thread::sleep_for(1s);
+    }
+}
