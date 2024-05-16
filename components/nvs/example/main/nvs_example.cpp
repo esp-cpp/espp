e751c0eb3560d2dff0215d1e1bb57e761d421865
SamAtBackbone <sam@playbackbone.com>
2024-05-16 08:25:38 -0700

    bugfix

--- components/nvs/example/main/nvs_example.cpp
+++ components/nvs/example/main/nvs_example.cpp
@@ -15,7 +15,8 @@ extern "C" void app_main(void) {
     std::error_code ec;
     uint8_t counter = 0;
     espp::Nvs nvs;
-    nvs.init();
+    nvs.init(ec);
+    ec.clear();
     //note that the namespace and key strings must be shorter than 15 characters
     nvs.get_or_set_var("system", "reset_counter", counter, counter, ec); 
     ec.clear();
