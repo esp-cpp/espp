9b6a2b8ec3114f20e1f7f3c83c6b1cfe3f1b2950
William Emfinger <waemfinger@gmail.com>
2024-05-17 08:59:46 -0500

    doc: fix nvs example case sensitivity

--- components/nvs/example/main/nvs_example.cpp
+++ components/nvs/example/main/nvs_example.cpp
@@ -9,35 +9,36 @@
 using namespace std::chrono_literals;
 
 extern "C" void app_main(void) {
-    std::this_thread::sleep_for(200ms);
-    fmt::print("Starting NVS example!\n");
-    //! [NVS example]
-    std::error_code ec;
-    uint8_t counter = 0;
-    espp::Nvs nvs;
+  std::this_thread::sleep_for(200ms);
+  fmt::print("Starting NVS example!\n");
+
+  //! [nvs example]
+  std::error_code ec;
+  uint8_t counter = 0;
+  espp::Nvs nvs;
+  nvs.init(ec);
+  ec.clear();
+  // note that the namespace and key strings must be <= 15 characters
+  nvs.get_or_set_var("system", "reset_counter", counter, counter, ec);
+  ec.clear();
+  fmt::print("Reset Counter = {}\n", counter);
+
+  counter++;
+
+  if (counter > 10) {
+    nvs.erase(ec);
     nvs.init(ec);
+    counter = 0;
+    fmt::print("NVS erased, Reset Counter set to 0\n");
     ec.clear();
-    //note that the namespace and key strings must be <= 15 characters
-    nvs.get_or_set_var("system", "reset_counter", counter, counter, ec); 
-    ec.clear();
-    fmt::print("Reset Counter = {}\n", counter);
+  }
 
-    counter ++;
+  nvs.set_var("system", "reset_counter", counter, ec);
+  fmt::print("Next Reset Counter will be = {}\n", counter);
+  fmt::print("NVS example complete!\n");
+  //! [nvs example]
 
-    if(counter > 10) {
-        nvs.erase(ec);
-        nvs.init(ec);
-        counter = 0;
-        fmt::print("NVS erased, Reset Counter set to 0\n");
-        ec.clear();
-    }
-
-    nvs.set_var("system", "reset_counter", counter, ec);
-    fmt::print("Next Reset Counter will be = {}\n", counter);
-    fmt::print("NVS example complete!\n");
-    //! [NVS example]
-
-    while (true) {
-        std::this_thread::sleep_for(1s);
-    }
+  while (true) {
+    std::this_thread::sleep_for(1s);
+  }
 }
