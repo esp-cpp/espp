b3f96a0af44138d59e139df97fc4c1585e4cfaff
Anne Brondijk <73667900+abrondijk@users.noreply.github.com>
2024-05-17 13:57:32 +0200

    fix: Added `CONFIG_BT_NIMBLE_ENABLED` check (#239)

--- components/gfps_service/src/gfps_service.cpp
+++ components/gfps_service/src/gfps_service.cpp
@@ -1,6 +1,10 @@
 #include "gfps_service.hpp"
 
+#if CONFIG_BT_NIMBLE_ENABLED || defined(_DOXYGEN_)
+
 const std::string espp::GfpsService::MODEL_ID_UUID = "fe2c1233-8366-4814-8eb0-01de32100bea";
 const std::string espp::GfpsService::KB_PAIRING_UUID = "fe2c1234-8366-4814-8eb0-01de32100bea";
 const std::string espp::GfpsService::PASSKEY_UUID = "fe2c1235-8366-4814-8eb0-01de32100bea";
 const std::string espp::GfpsService::ACCOUNT_KEY_UUID = "fe2c1236-8366-4814-8eb0-01de32100bea";
+
+#endif
