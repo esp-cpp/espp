2ce52482770d47b7112fdda0b65e59c3a7d4f8da
William Emfinger <waemfinger@gmail.com>
2024-05-18 14:05:36 -0500

    feat(gt911): add support for reading home button (#243)
    * feat(gt911): add support for reading home button
    
    * readme: update

--- components/gt911/example/main/gt911_example.cpp
+++ components/gt911/example/main/gt911_example.cpp
@@ -18,13 +18,24 @@ extern "C" void app_main(void) {
         .port = I2C_NUM_0,
         .sda_io_num = (gpio_num_t)CONFIG_EXAMPLE_I2C_SDA_GPIO,
         .scl_io_num = (gpio_num_t)CONFIG_EXAMPLE_I2C_SCL_GPIO,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .timeout_ms = 100,
+        .clk_speed = 400 * 1000,
     });
+
+    bool has_gt911_5d = i2c.probe_device(0x5d);
+    bool has_gt911_14 = i2c.probe_device(0x14);
+    uint8_t address = has_gt911_5d ? 0x5d : 0x14;
+    fmt::print("Touchpad probe: {}\n", has_gt911_5d || has_gt911_14);
+    fmt::print("       address: {:#02x}\n", address);
 
     // now make the gt911 which decodes the data
     espp::Gt911 gt911({.write = std::bind(&espp::I2c::write, &i2c, std::placeholders::_1,
                                           std::placeholders::_2, std::placeholders::_3),
                        .read = std::bind(&espp::I2c::read, &i2c, std::placeholders::_1,
                                          std::placeholders::_2, std::placeholders::_3),
+                       .address = address,
                        .log_level = espp::Logger::Verbosity::WARN});
 
     // and finally, make the task to periodically poll the gt911 and print
@@ -41,6 +52,9 @@ extern "C" void app_main(void) {
         return false; // don't stop the task
       }
       // get the state
+      bool home_pressed = false;
+      home_pressed = gt911.get_home_button_state();
+      fmt::print("home_pressed: {}\n", home_pressed);
       uint8_t num_touch_points = 0;
       uint16_t x = 0, y = 0;
       gt911.get_touch_point(&num_touch_points, &x, &y);
