cb443d750d7a44dc9cf95972c09d40fd93bbc05b
William Emfinger <waemfinger@gmail.com>
2024-06-05 10:27:54 -0500

    feat(adc): add print helpers for esp-idf adc types (#254)
    * feat(adc): add print helpers for esp-idf adc types
    * Add missing include to adc_types.hpp
    * Add format helpers for adc_unit_t, adc_channel_t, and adc_atten_t
    * Update format helper for espp::AdcConfig to use other helpers instead of casting to int
    
    * update deprecated atten value

--- components/adc/include/adc_types.hpp
+++ components/adc/include/adc_types.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <driver/adc.h>
+
 #include "format.hpp"
 
 namespace espp {
@@ -20,12 +22,43 @@ static bool operator!=(const AdcConfig &lhs, const AdcConfig &rhs) {
 static bool operator==(const AdcConfig &lhs, const AdcConfig &rhs) { return !(lhs != rhs); }
 } // namespace espp
 
+// for libfmt printing of adc_unit_t
+template <> struct fmt::formatter<adc_unit_t> : fmt::formatter<std::string> {
+  template <typename FormatContext> auto format(adc_unit_t t, FormatContext &ctx) {
+    return fmt::format_to(ctx.out(), "ADC_UNIT_{}", (int)t + 1);
+  }
+};
+
+// for libfmt printing of adc_channel_t
+template <> struct fmt::formatter<adc_channel_t> : fmt::formatter<std::string> {
+  template <typename FormatContext> auto format(adc_channel_t t, FormatContext &ctx) {
+    return fmt::format_to(ctx.out(), "ADC_CHANNEL_{}", (int)t);
+  }
+};
+
+// for libfmt printing of adc_atten_t
+template <> struct fmt::formatter<adc_atten_t> : fmt::formatter<std::string> {
+  template <typename FormatContext> auto format(adc_atten_t t, FormatContext &ctx) {
+    switch (t) {
+    case ADC_ATTEN_DB_0:
+      return fmt::format_to(ctx.out(), "ADC_ATTEN_DB_0");
+    case ADC_ATTEN_DB_2_5:
+      return fmt::format_to(ctx.out(), "ADC_ATTEN_DB_2_5");
+    case ADC_ATTEN_DB_6:
+      return fmt::format_to(ctx.out(), "ADC_ATTEN_DB_6");
+    case ADC_ATTEN_DB_12:
+      return fmt::format_to(ctx.out(), "ADC_ATTEN_DB_12");
+    }
+    return fmt::format_to(ctx.out(), "ADC_ATTEN_UNKNOWN");
+  }
+};
+
 // for easy serialization of AdcConfig with libfmt
 template <> struct fmt::formatter<espp::AdcConfig> {
   template <typename ParseContext> constexpr auto parse(ParseContext &ctx) { return ctx.begin(); }
 
   template <typename FormatContext> auto format(const espp::AdcConfig &c, FormatContext &ctx) {
-    return fmt::format_to(ctx.out(), "AdcConfig(unit={}, channel={}, attenuation={})", (int)c.unit,
-                          (int)c.channel, (int)c.attenuation);
+    return fmt::format_to(ctx.out(), "AdcConfig(unit={}, channel={}, attenuation={})", c.unit,
+                          c.channel, c.attenuation);
   }
 };
