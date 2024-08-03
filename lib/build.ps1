0c069e16e969164e1aa6c49da56894416bfa0188
William Emfinger <waemfinger@gmail.com>
2024-08-03 09:05:57 -0500

    update to use pybind11_add_module to simplify cmake lists, add windows build script, and update to build for release

---
+++ lib/build.ps1
@@ -0,0 +1,19 @@
+# powershell script to build the project using cmake
+
+# Create build directory if it doesn't exist
+$buildDir = "build"
+if (-not (Test-Path -Path $buildDir)) {
+    New-Item -ItemType Directory -Path $buildDir
+}
+
+# Change to the build directory
+Set-Location -Path $buildDir
+
+# Run cmake
+cmake ..
+
+# Run cmake --build . --config Release --target install
+cmake --build . --config Release --target install
+
+# Change back to the original directory
+Set-Location -Path ..
--- lib/build.sh
+++ lib/build.sh
@@ -3,5 +3,4 @@
 mkdir build
 cd build
 cmake ..
-cmake --build .
-cmake --install .
+cmake --build . --config Release --target install
