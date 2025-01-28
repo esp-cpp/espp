7901b0d060295f3f48d35905915ec55d87388ae7
William Emfinger <waemfinger@gmail.com>
2025-01-28 13:02:42 -0600

    WIP testing example more

--- components/binary-log/example/main/binary_log_example.cpp
+++ components/binary-log/example/main/binary_log_example.cpp
@@ -2,16 +2,36 @@
 #include <functional>
 #include <thread>
 
+#include <esp_core_dump.h>
+
 #include "binary-log.hpp"
+#include "file_system.hpp"
+#include "format.hpp"
 
 using namespace std::chrono_literals;
 
 extern "C" void app_main(void) {
+  fmt::print("Starting binary log example\n");
+  // initialize the file system
+  std::error_code ec;
+  auto &fs = espp::FileSystem::get();
+  auto root = fs.get_root_path();
+  const std::filesystem::path logfile = root / std::filesystem::path{"log.out"};
+
+  // first see if the file exists
+  if (std::filesystem::exists(logfile, ec)) {
+    fmt::print("Removing existing log file\n");
+    fs.remove(logfile, ec);
+  }
+
   {
     //! [Binary Log example]
-    float num_seconds_to_run = 10.0f;
-    // create loggers
-    binary_log::binary_log log("log.out");
+    float num_seconds_to_run = 3.0f;
+    // create logger
+    fmt::print("Creating binary log file: {}\n", logfile.c_str());
+    binary_log::binary_log log(logfile.c_str());
+
+    fmt::print("Starting binary log\n");
     auto start = std::chrono::high_resolution_clock::now();
     auto now = std::chrono::high_resolution_clock::now();
     float elapsed = std::chrono::duration<float>(now - start).count();
@@ -29,5 +49,32 @@ extern "C" void app_main(void) {
       std::this_thread::sleep_for(500ms);
     }
     //! [Logger example]
+  }
+
+  // now print out the logger files
+  fmt::print("Printing log file\n");
+
+  size_t file_size = std::filesystem::file_size(logfile, ec);
+  std::ifstream ifs(logfile, std::ios::in | std::ios::binary);
+  // read bytes
+  std::vector<char> file_bytes(file_size);
+  ifs.read(file_bytes.data(), file_size);
+  ifs.close();
+  fmt::print("Read {} bytes from log file\n", file_size);
+  fmt::print("File contents:\n");
+  fmt::print("\t{::02x}\n", file_bytes);
+
+  // print the contents of the file system
+  fmt::print("{}\n", fs.list_directory(root, {}));
+
+  // now erase the file system
+  fmt::print("Erase file system\n");
+  fs.remove_contents(root, ec);
+
+  fmt::print("Finished binary log example\n");
+
+  // now loop forever
+  while (true) {
+    std::this_thread::sleep_for(1s);
   }
 }
