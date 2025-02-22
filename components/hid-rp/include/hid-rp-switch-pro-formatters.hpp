96edbbb1736e38ecbc3b4e35b6499388ba6a707a
William Emfinger <waemfinger@gmail.com>
2025-02-21 23:31:52 -0600

    feat(hid-rp): Flesh out gamepads to fill out their interfaces (#381)
    * Add some concepts for structs to make it easier to get / set buttons with custom / user-defined structs/classes without having to subclass
    * Add more getters / setters for each Switch and Xbox
    * Add specializations to the Xbox class to explicitly match buttons for the xbox controller
    
    Makes using the controllers (and converting between controller report types) easier / more painless
    
    Build and run `hid-rp/example`
    Build and run within [esp-usb-ble-hid](https://github.com/finger563/esp-usb-ble-hid)

--- components/hid-rp/include/hid-rp-switch-pro-formatters.hpp
+++ components/hid-rp/include/hid-rp-switch-pro-formatters.hpp
@@ -2,20 +2,19 @@
 
 #include "format.hpp"
 
-template<uint8_t REPORT_ID>
-struct fmt::formatter<espp::SwitchProGamepadInputReport<REPORT_ID>> {
+template <uint8_t REPORT_ID> struct fmt::formatter<espp::SwitchProGamepadInputReport<REPORT_ID>> {
   template <typename ParseContext> constexpr auto parse(ParseContext &ctx) const {
     return ctx.begin();
   }
 
   template <typename FormatContext>
-  auto
-  format(const espp::SwitchProGamepadInputReport<REPORT_ID> &report,
-         FormatContext &ctx) const {
+  auto format(const espp::SwitchProGamepadInputReport<REPORT_ID> &report,
+              FormatContext &ctx) const {
     auto out = ctx.out();
     fmt::format_to(out, "SwitchProGamepadInputReport {{");
     fmt::format_to(out, "analog: {::#04x}", report.analog);
-    uint32_t buttons = report.raw_report[4] << 16 | report.raw_report[3] << 8 | report.raw_report[2];
+    uint32_t buttons =
+        report.raw_report[4] << 16 | report.raw_report[3] << 8 | report.raw_report[2];
     fmt::format_to(out, "], buttons: [{:#026b}]", buttons);
     return fmt::format_to(out, "}}");
   }
