957213f000893f474ef6e97df63af21ad8c04d18
William Emfinger <waemfinger@gmail.com>
2025-03-03 15:54:42 -0600

    remove goto from example

--- components/led_strip/example/main/led_strip_example.cpp
+++ components/led_strip/example/main/led_strip_example.cpp
@@ -84,9 +84,11 @@ extern "C" void app_main(void) {
                 }
                 if (session_state & RMT_ENCODING_MEM_FULL) {
                   state |= RMT_ENCODING_MEM_FULL;
-                  goto out; // yield if there's no free space for encoding artifacts
+                  // skip the fallthrough to leave the switch and finish the
+                  // function
+                  break;
                 }
-                // fall-through
+                // intentional fall-through
               case 1: // send reset code
                 encoded_symbols += copy_encoder->encode(copy_encoder, channel, &led_reset_code,
                                                         sizeof(led_reset_code), &session_state);
@@ -96,10 +98,11 @@ extern "C" void app_main(void) {
                 }
                 if (session_state & RMT_ENCODING_MEM_FULL) {
                   state |= RMT_ENCODING_MEM_FULL;
-                  goto out; // yield if there's no free space for encoding artifacts
+                  // skip the fallthrough to leave the switch and finish the
+                  // function
+                  break;
                 }
               }
-            out:
               *ret_state = static_cast<rmt_encode_state_t>(state);
               return encoded_symbols;
             },
