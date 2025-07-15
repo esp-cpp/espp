8f88d138901082154651922114e5597c0dabf6d7
William Emfinger <waemfinger@gmail.com>
2025-07-15 12:56:03 -0500

    feat(ws-s3-geek): Add board support package (BSP) for the Waveshare ESP32-S3 GEEK (#481)
    * feat(ws-s3-geek): Add board support package (BSP) for the Waveshare ESP32-S3 GEEK
    
    * readme: update
    
    * fix sa
    
    * fix doc

---
+++ components/ws-s3-geek/example/README.md
@@ -0,0 +1,84 @@
+# Waveshare ESP32-S3-GEEK Example
+
+This example shows how to use the `espp::WsS3Geek` hardware abstraction
+component initialize the components on the Waveshare ESP32-S3-GEEK.
+
+It initializes the display, and button subsystems.
+
+<img width="1260" height="949" alt="image" src="https://github.com/user-attachments/assets/0f0b051a-701d-47ea-8674-1c8847f8e921" />
+
+## How to use example
+
+### Hardware Required
+
+This example is designed to run on the [Waveshare
+ESP32-S3-GEEK](https://www.waveshare.com/wiki/ESP32-S3-GEEK) dev board which
+has:
+
+* WiFi / BLE
+* uSD card
+* Color TFT LCD (ST7789P3, 240x135 1.14" IPS LCD)
+* Boot Button
+
+##### Waveshare ESP32-S3-GEEK Pin Configuration
+
+| LCD Pin    | ESP32S3 IO Pin Number |
+|------------|-----------------------|
+| CS         | 10                    |
+| SDA (MOSI) | 11                    |
+| SCL (CLK)  | 12                    |
+| DC         | 8                     |
+| Reset      | 9                     |
+| Backlight  | 7                     |
+
+| TF / uSD Card Pin | ESP32S3 IO Pin Number |
+|-------------------|-----------------------|
+| D0 / MISO         | 37                    |
+| D1                | 33                    |
+| D2                | 38                    |
+| D3 / CS           | 34                    |
+| CLK               | 36                    |
+| CMD / MOSI        | 35                    |
+
+
+### Build and Flash
+
+Build the project and flash it to the board, then run monitor tool to view
+serial output:
+
+```
+idf.py -p PORT flash monitor
+```
+
+(Replace PORT with the name of the serial port to use.)
+
+(To exit the serial monitor, type ``Ctrl-]``.)
+
+See the Getting Started Guide for full steps to configure and use ESP-IDF to build projects.
+
+## Example Output
+
+```console
+[WsS3Geek Example/I][0.026]: Starting example!
+[WsS3Geek/I][0.032]: Initializing LCD...
+I (357) gpio: GPIO[8]| InputEn: 0| OutputEn: 1| OpenDrain: 0| Pullup: 0| Pulldown: 0| Intr:0
+I (365) gpio: GPIO[9]| InputEn: 0| OutputEn: 1| OpenDrain: 0| Pullup: 0| Pulldown: 0| Intr:0
+[WsS3Geek/I][0.453]: Initializing display with pixel buffer size: 12000 bytes
+W (776) ledc: the binded timer can't keep alive in sleep
+[WsS3Geek/I][0.458]: Initializing SD card
+I (782) gpio: GPIO[34]| InputEn: 0| OutputEn: 1| OpenDrain: 0| Pullup: 0| Pulldown: 0| Intr:0
+I (830) sdspi_transaction: cmd=52, R1 response: command not supported
+I (872) sdspi_transaction: cmd=5, R1 response: command not supported
+[WsS3Geek/I][0.576]: Filesystem mounted
+Name: 00000
+Type: SDHC
+Speed: 20.00 MHz (limit: 20.00 MHz)
+Size: 30543MB
+CSD: ver=2, sector_size=512, capacity=62552064 read_bl_len=9
+SSR: bus_width=1
+[WsS3Geek Example/I][0.583]: Initializing the button
+[WsS3Geek/I][0.589]: Initializing button
+I (915) gpio: GPIO[0]| InputEn: 1| OutputEn: 0| OpenDrain: 0| Pullup: 1| Pulldown: 0| Intr:3
+```
+
+<img width="709" height="379" alt="CleanShot 2025-07-15 at 11 47 26" src="https://github.com/user-attachments/assets/f22711b3-ce37-4084-99b1-0afc8dbbbd12" />
---
+++ components/ws-s3-geek/src/sdcard.cpp
@@ -0,0 +1,102 @@
+#include "ws-s3-geek.hpp"
+
+using namespace espp;
+
+/////////////////////////////////////////////////////////////////////////////
+// uSD Card
+/////////////////////////////////////////////////////////////////////////////
+
+bool WsS3Geek::initialize_sdcard(const WsS3Geek::SdCardConfig &config) {
+  if (sdcard_) {
+    logger_.error("SD card already initialized!");
+    return false;
+  }
+
+  logger_.info("Initializing SD card");
+
+  esp_err_t ret;
+  // Options for mounting the filesystem. If format_if_mount_failed is set to
+  // true, SD card will be partitioned and formatted in case when mounting
+  // fails.
+  esp_vfs_fat_sdmmc_mount_config_t mount_config;
+  memset(&mount_config, 0, sizeof(mount_config));
+  mount_config.format_if_mount_failed = config.format_if_mount_failed;
+  mount_config.max_files = config.max_files;
+  mount_config.allocation_unit_size = config.allocation_unit_size;
+
+  spi_bus_config_t bus_cfg;
+  memset(&bus_cfg, 0, sizeof(bus_cfg));
+  bus_cfg.mosi_io_num = sdcard_mosi;
+  bus_cfg.miso_io_num = sdcard_miso;
+  bus_cfg.sclk_io_num = sdcard_clk;
+  bus_cfg.quadwp_io_num = -1;
+  bus_cfg.quadhd_io_num = -1;
+  bus_cfg.max_transfer_sz = frame_buffer_size * sizeof(lv_color_t) + 100;
+  ret = spi_bus_initialize(sdcard_spi_num, &bus_cfg,
+                           SDSPI_DEFAULT_DMA); // SPI_DMA_CH_AUTO); // SDSPI_DEFAULT_DMA);
+  if (ret != ESP_OK) {
+    logger_.error("Failed to initialize bus.");
+    return false;
+  }
+
+  // By default, SD card frequency is initialized to SDMMC_FREQ_DEFAULT (20MHz)
+  // For setting a specific frequency, use host.max_freq_khz (range 400kHz - 20MHz for SDSPI)
+  // Example: for fixed frequency of 10MHz, use host.max_freq_khz = 10000;
+  sdmmc_host_t host = SDSPI_HOST_DEFAULT();
+  host.slot = sdcard_spi_num; // Use SPI1 host
+  // host.max_freq_khz = 20 * 1000;
+
+  // This initializes the slot without card detect (CD) and write protect (WP) signals.
+  // Modify slot_config.gpio_cd and slot_config.gpio_wp if your board has these signals.
+  spi_host_device_t host_id = (spi_host_device_t)host.slot;
+  sdspi_device_config_t slot_config = SDSPI_DEVICE_CONFIG_DEFAULT();
+  slot_config.gpio_cs = sdcard_cs;
+  slot_config.host_id = host_id;
+
+  logger_.debug("Mounting filesystem");
+  ret = esp_vfs_fat_sdspi_mount(mount_point, &host, &slot_config, &mount_config, &sdcard_);
+
+  // // Use settings defined above to initialize SD card and mount FAT filesystem.
+  // // Note: esp_vfs_fat_sdmmc/sdspi_mount is all-in-one convenience functions.
+  // // Please check its source code and implement error recovery when developing
+  // // production applications.
+  // logger_.debug("Using SDMMC peripheral");
+
+  // // By default, SD card frequency is initialized to SDMMC_FREQ_DEFAULT (20MHz)
+  // // For setting a specific frequency, use host.max_freq_khz (range 400kHz - 20MHz for SDSPI)
+  // sdmmc_host_t host = SDMMC_HOST_DEFAULT();
+  // host.max_freq_khz = SDMMC_FREQ_DEFAULT;
+
+  // // This initializes the slot without card detect (CD) and write protect (WP) signals.
+  // // Modify slot_config.gpio_cd and slot_config.gpio_wp if your board has these signals.
+  // sdmmc_slot_config_t slot_config = SDMMC_SLOT_CONFIG_DEFAULT();
+  // slot_config.clk = sdcard_clk;
+  // slot_config.cmd = sdcard_cmd;
+  // slot_config.d0 = sdcard_d0;
+  // slot_config.d1 = sdcard_d1;
+  // slot_config.d2 = sdcard_d2;
+  // slot_config.d3 = sdcard_d3;
+
+  // logger_.debug("Mounting filesystem");
+  // ret = esp_vfs_fat_sdmmc_mount(mount_point, &host, &slot_config, &mount_config, &sdcard_);
+
+  if (ret != ESP_OK) {
+    if (ret == ESP_FAIL) {
+      logger_.error("Failed to mount filesystem. ");
+      return false;
+    } else {
+      logger_.error("Failed to initialize the card ({}). "
+                    "Make sure SD card lines have pull-up resistors in place.",
+                    esp_err_to_name(ret));
+      return false;
+    }
+    return false;
+  }
+
+  logger_.info("Filesystem mounted");
+
+  // Card has been initialized, print its properties
+  sdmmc_card_print_info(stdout, sdcard_);
+
+  return true;
+}
