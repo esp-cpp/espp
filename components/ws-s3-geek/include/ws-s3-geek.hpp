04b529f26699ae6d85586ac5335bab7491fd4a13
William Emfinger <waemfinger@gmail.com>
2025-08-12 10:45:39 -0500

    feat(ws-s3-geek): Backlight is available after initialize_lcd now (#493)

--- components/ws-s3-geek/include/ws-s3-geek.hpp
+++ components/ws-s3-geek/include/ws-s3-geek.hpp
@@ -16,6 +16,7 @@
 
 #include "base_component.hpp"
 #include "interrupt.hpp"
+#include "led.hpp"
 #include "st7789.hpp"
 
 namespace espp {
@@ -103,10 +104,12 @@ public:
 
   /// Set the brightness of the backlight
   /// \param brightness The brightness of the backlight as a percentage (0 - 100)
+  /// \note This function will only work after initialize_lcd() has been called
   void brightness(float brightness);
 
   /// Get the brightness of the backlight
   /// \return The brightness of the backlight as a percentage (0 - 100)
+  /// \note This function will only work after initialize_lcd() has been called
   float brightness() const;
 
   /// Get the VRAM 0 pointer (DMA memory used by LVGL)
@@ -247,6 +250,8 @@ protected:
 
   // display
   std::shared_ptr<Display<Pixel>> display_;
+  std::vector<Led::ChannelConfig> backlight_channel_configs_{};
+  std::shared_ptr<Led> backlight_{};
   /// SPI bus for communication with the LCD
   spi_bus_config_t lcd_spi_bus_config_;
   spi_device_interface_config_t lcd_config_;
