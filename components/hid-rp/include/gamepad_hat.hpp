2506278a3fe618d318b0d8a8ecd27a5de60a0dfc
William Emfinger <waemfinger@gmail.com>
2025-09-22 17:06:22 -0500

    define storage type for enum calss

--- components/hid-rp/include/gamepad_hat.hpp
+++ components/hid-rp/include/gamepad_hat.hpp
@@ -5,7 +5,7 @@
 namespace espp {
 namespace gamepad {
 /// Possible Hat switch directions
-enum class Hat {
+enum class Hat : std::uint8_t {
   CENTERED = 0x0f, ///< Centered, no direction pressed.
   UP = 1,
   UP_RIGHT,
